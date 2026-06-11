00527b2bcc33cb2a005bf3b7a6c48ca259f97e07
deepika paneer <deepselvam26@gmail.com>
2026-06-11 12:23:25 -0400

    Improved SSH connection script with configurable usename

--- scripts/test_ssh_connection.sh
+++ scripts/test_ssh_connection.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 SERVER_IP="$1"
+SSH_USER="$2"
 SSH_KEY="$HOME/.ssh/secure_ssh_project_key"
 
-if [ -z "$SERVER_IP" ]; then
-  echo "Usage: ./test_ssh_connection.sh <server-ip>"
+if [ -z "$SERVER_IP" ] || [ -z "$SSH_USER" ]; then
+  echo "Usage: ./test_ssh_connection.sh <server-ip> <username>"
   exit 1
 fi
 
@@ -13,7 +14,7 @@ echo "Testing SSH connection to <private-ip>"
 ssh -i "$SSH_KEY" \
     -o BatchMode=yes \
     -o ConnectTimeout=5 \
-    deepika@"$SERVER_IP" "hostname"
+    "$SSH_USER"@"$SERVER_IP" "hostname"
 
 if [ $? -eq 0 ]; then
     echo "SSH connection successful."
